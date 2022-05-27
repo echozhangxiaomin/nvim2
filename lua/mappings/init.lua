c1df8b0c4bbcae7b4640dfc51b418e46974005a6
echozhangxiaomin <xiaomin.zhang@pplingo.com>
2022-05-27 13:39:09 +0800

    tnoremap

--- lua/mappings/init.lua
+++ lua/mappings/init.lua
@@ -9,6 +9,7 @@ vim.api.nvim_set_keymap('n','<C-r>',':Telescope<CR>',{noremap =true,silent = tru
 vim.api.nvim_set_keymap('n','<M-x>',':Telescope<CR>',{noremap =true,silent = true})
 vim.api.nvim_set_keymap('n','-','$',{noremap = true,silent = true})
 vim.api.nvim_set_keymap('n','<leader>s',':FloatermToggle<CR>',{noremap = true,silent = true})
+vim.api.nvim_set_keymap('t','<leader>s','<C-\\><C-n>:FloatermToggle<CR>',{silent = true})
 vim.api.nvim_set_keymap('n','H',':vertical resize+10<CR>',{noremap = true,silent = true})
 vim.api.nvim_set_keymap('n','J',':resize-10<CR>',{noremap = true,silent = true})
 vim.api.nvim_set_keymap('n','K',':resize+10<CR>',{noremap = true,silent = true})
