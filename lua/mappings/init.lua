e0d2b62a81ea466001be6a7f3dd596e346a63fbd
echozhangxiaomin <xiaomin.zhang@pplingo.com>
2022-06-05 05:32:13 +0800

    keymap

--- lua/mappings/init.lua
+++ lua/mappings/init.lua
@@ -1,47 +1,39 @@
 vim.g.mapleader = ' '
-
-vim.api.nvim_set_keymap('n', '<c-s>', ':w<CR>', {})
-vim.api.nvim_set_keymap('n', ';', ':', {})
-vim.api.nvim_set_keymap('i', '<c-j>', 'copilot#Accept("\\<CR>")', { silent = true, script = true, expr = true })
-vim.api.nvim_set_keymap('i', '<c-s>', '<ESC>:w<CR>a', {})
-vim.api.nvim_set_keymap('i', '<c-p>', '<up>', { noremap = true, silent = true, nowait = true })
-vim.api.nvim_set_keymap('i', '<c-n>', '<down>', { noremap = true, silent = true, nowait = true })
-vim.api.nvim_set_keymap('i', '<c-b>', '<left>', { noremap = true, silent = true, nowait = true })
-vim.api.nvim_set_keymap('i', '<M-b>', '<S-left>', { noremap = true, silent = true, nowait = true })
-vim.api.nvim_set_keymap('i', '<c-f>', '<right>', { noremap = true, silent = true, nowait = true })
-vim.api.nvim_set_keymap('i', '<M-f>', '<S-right>', { noremap = true, silent = true, nowait = true })
-vim.api.nvim_set_keymap('i', '<c-k>', '<C-o>D', { noremap = true, silent = true, nowait = true })
-vim.api.nvim_set_keymap('i', '<M-d>', '<C-o>dw', { noremap = true, silent = true, nowait = true })
-vim.api.nvim_set_keymap('i', '<c-e>', '<end>', { noremap = true, silent = true, nowait = true })
-vim.api.nvim_set_keymap('i', '<c-a>', '<home>', { noremap = true, silent = true, nowait = true })
-vim.api.nvim_set_keymap('i', '<c-d>', '<del>', { noremap = true, silent = true, nowait = true })
-vim.api.nvim_set_keymap('n', '<leader>n', '<cmd>NvimTreeToggle<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>r', '<cmd>Telescope live_grep<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>f', '<cmd>Telescope find_files prompt_prefix=🔍 <CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<C-r>', '<cmd>Telescope<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<M-x>', '<cmd>Telescope<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '-', '$', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>s', '<cmd>FloatermToggle <CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>g', '<cmd>FloatermNew --width=0.8 --height=0.8 lazygit<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<space>/', '<cmd>Commentary<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('v', '<space>/', '<cmd>Commentary<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('t', '<esc>', '<C-\\><C-n>', { silent = true })
-vim.api.nvim_set_keymap('n', 'H', '<cmd>vertical resize+10<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', 'J', '<cmd>resize-10<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', 'K', '<cmd>resize+10<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', 'L', '<cmd>vertical resize-10<CR>', { noremap = true, silent = true })
-
-
 local map = vim.api.nvim_set_keymap
-local opts = { noremap = true, silent = true }
-
--- Move to previous/next
+local opts = { noremap = true, silent = true, nowait = true }
+map('', '<c-p>', '<up>', opts)
+map('', '<c-n>', '<down>', opts)
+map('', '<c-b>', '<left>', opts)
+map('', '<M-b>', '<S-left>', opts)
+map('', '<c-f>', '<right>', opts)
+map('', '<M-f>', '<S-right>', opts)
+map('', '<c-e>', '<end>', opts)
+map('', '<c-a>', '<home>', opts)
+map('', '<c-d>', '<del>', opts)
+map('n', '<c-s>', ':w<CR>', opts)
+map('n', ';', ':', {})
+map('i', '<c-j>', 'copilot#Accept("\\<CR>")', opts)
+map('i', '<c-s>', '<ESC>:w<CR>a', opts)
+map('i', '<c-k>', '<C-o>D', opts)
+map('i', '<M-d>', '<C-o>dw', opts)
+map('n', '<leader>n', '<cmd>NvimTreeToggle<CR>', opts)
+map('n', '<leader>r', '<cmd>Telescope live_grep<CR>', opts)
+map('n', '<leader>f', '<cmd>Telescope find_files prompt_prefix=🔍 <CR>', opts)
+map('n', '<C-r>', '<cmd>Telescope<CR>', opts)
+map('n', '<M-x>', '<cmd>Telescope<CR>', opts)
+map('n', '<leader>s', '<cmd>FloatermToggle <CR>', opts)
+map('n', '<leader>g', '<cmd>FloatermNew --width=0.8 --height=0.8 lazygit<CR>', opts)
+map('n', '<space>/', '<cmd>Commentary<CR>', opts)
+map('v', '<space>/', '<cmd>Commentary<CR>', opts)
+map('t', '<esc>', '<C-\\><C-n>', opts)
+map('n', 'H', '<cmd>vertical resize+10<CR>', opts)
+map('n', 'J', '<cmd>resize-10<CR>', opts)
+map('n', 'K', '<cmd>resize+10<CR>', opts)
+map('n', 'L', '<cmd>vertical resize-10<CR>', opts)
 map('n', '<A-,>', ':BufferPrevious<CR>', opts)
 map('n', '<A-.>', ':BufferNext<CR>', opts)
--- Re-order to previous/next
 map('n', '<A-<>', ':BufferMovePrevious<CR>', opts)
 map('n', '<A->>', ' :BufferMoveNext<CR>', opts)
--- Goto buffer in position...
 map('n', '<A-1>', ':BufferGoto 1<CR>', opts)
 map('n', '<A-2>', ':BufferGoto 2<CR>', opts)
 map('n', '<A-3>', ':BufferGoto 3<CR>', opts)
@@ -52,26 +44,9 @@ map('n', '<A-7>', ':BufferGoto 7<CR>', opts)
 map('n', '<A-8>', ':BufferGoto 8<CR>', opts)
 map('n', '<A-9>', ':BufferGoto 9<CR>', opts)
 map('n', '<A-0>', ':BufferLast<CR>', opts)
--- Pin/unpin buffer
 map('n', '<A-p>', ':BufferPin<CR>', opts)
--- Close buffer
 map('n', '<A-c>', ':BufferClose<CR>', opts)
--- Wipeout buffer
---                 :BufferWipeout<CR>
--- Close commands
---                 :BufferCloseAllButCurrent<CR>
---                 :BufferCloseAllButPinned<CR>
---                 :BufferCloseAllButCurrentOrPinned<CR>
---                 :BufferCloseBuffersLeft<CR>
---                 :BufferCloseBuffersRight<CR>
--- Magic buffer-picking mode
--- map('n', '<C-p>', ':BufferPick<CR>', opts)
--- Sort automatically by...
 map('n', '<Space>bb', ':BufferOrderByBufferNumber<CR>', opts)
 map('n', '<Space>bd', ':BufferOrderByDirectory<CR>', opts)
 map('n', '<Space>bl', ':BufferOrderByLanguage<CR>', opts)
 map('n', '<Space>bw', ':BufferOrderByWindowNumber<CR>', opts)
-
--- Other:
--- :BarbarEnable - enables barbar (enabled by default)
--- :BarbarDisable - very bad command, should never be used
