34aecc29fa20a18225013485d552f66eac0c340f
echozhangxiaomin <xiaomin.zhang@pplingo.com>
2022-06-13 13:45:37 +0800

    orgmode

--- lua/orgmode-config/init.lua
+++ lua/orgmode-config/init.lua
@@ -5,3 +5,5 @@ require('orgmode').setup({
     org_agenda_files = { '~/org/*', '~/orgs/**/*' },
     org_default_notes_file = '~/org/refile.org',
 })
+
+vim.cmd('let g:copilot_filetype = {"org":v:false}')
